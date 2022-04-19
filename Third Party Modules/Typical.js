aea4b2ed346c08efc1ba9d63c40e4ee3e063e114
Tangilur Rahman <mohammadtangilurrahman@gmail.com>
2022-04-19 17:03:48 +0600

    add typical module

---
+++ Third Party Modules/Typical.js
@@ -0,0 +1,33 @@
+/*
+❤️❤️❤️ Typical ❤️❤️❤️
+
+  🌿 We can create easily typing animation-effect by using that third-party module.
+
+
+  🌿 Install 
+
+    > npm i react-typical
+
+    > import Typical from "react-typical"
+  
+
+  Example :  📝 
+
+    <Typical
+
+      loop={Infinity}
+
+      steps={["Hello", 1000, "Hello world!", 500]}
+        
+    />
+
+
+    👉 We can insert multiple steps in that Array.
+
+    👉 After every steps, next element define how much millisecond delay to start next step.
+     
+
+
+  😊 END 😊  
+
+*/
