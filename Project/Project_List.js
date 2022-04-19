fc51c71a611ccaed7cf7b76d6dc541224cd83707
Tangilur Rahman <mohammadtangilurrahman@gmail.com>
2022-04-19 12:34:19 +0600

    add navbar

--- Project/Project_List.js
+++ Project/Project_List.js
@@ -1,10 +1,11 @@
 /*
-
+❤️Project List❤️
 
 1. Raw Node JS Project
 2. MVC Architecture Project
 3. ToDo - Mongoose
 4. JWT - LOGIN System - Mongoose
 5. Login-System - Mern
+6. Responsible Navbar
 
 */
